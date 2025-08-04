b7d6b72bbc525a4960533386968a4509ad8d11db
venistbim <venistbim@gmail.com>
2025-08-04 15:31:00 +0400

    Add project files.

---
+++ If else programming exercises/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else_programming_exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter first value: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second value :");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            if (a > b)
+            {
+                Console.WriteLine("a is maximum");
+
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("b is maximum");
+            }
+
+            //else if (b == a)
+
+            //{
+            //    Console.WriteLine("both are equal");
+            //}
+
+            else 
+            {
+                Console.WriteLine("Invalid input");
+            
+            }
+
+
+                Console.ReadLine();
+
+
+
+
+        }
+    }
+}
