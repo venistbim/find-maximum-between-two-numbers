cf572030916ab0aeb33ff65c1b020f65d8aa1be7
venistbim <venistbim@gmail.com>
2025-08-05 23:32:24 +0400

    if else code

---
+++ check positive, negative or zero/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_positive__negative_or_zero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int Value = 0;
+
+            Console.WriteLine("enter a values: ");
+            Value = Convert.ToInt32(Console.ReadLine());
+            
+            if (Value > 0)
+            {
+                Console.WriteLine($"{Value} is positive");
+            }
+            else if (Value < 0)
+            {
+                Console.WriteLine($"{Value} is negative");
+            }
+
+            else
+            {
+                Console.WriteLine("value is zero");
+       
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
